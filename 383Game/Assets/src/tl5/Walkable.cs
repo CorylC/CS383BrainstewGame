6d4bddbe0ea38a942c4af19fa520e05f216695ba
Aneesha-Shr <109557810+Aneesha-Shr@users.noreply.github.com>
2025-02-24 13:55:38 -0800

    Ground Unit AI Updated

---
+++ 383Game/Assets/src/tl5/Walkable.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Walkable : MonoBehaviour
+{
+
+    private const float ForcePower = 10f;
+
+    public new Rigidbody2D rigidbody;
+
+    public float speed = 2f;
+    public float force = 2f;
+
+    private Vector2 direction;
+
+    public void MoveTo (Vector2 direction) {
+        this.direction = direction;
+    }
+
+    public void Stop() {
+        MoveTo(Vector2.zero);
+    }
+
+    private void FixedUpdate() {
+        var desiredVelocity = direction * speed;
+        var deltaVelocity = desiredVelocity - rigidbody.linearVelocity;
+        Vector3 moveForce = deltaVelocity * (force * ForcePower * Time.fixedDeltaTime);
+
+        rigidbody.AddForce(moveForce);
+
+        Debug.Log($"Direction: {direction}, Velocity: {rigidbody.linearVelocity}");
+    }
+
+
+
+}
+
+
