6616ea5088b44c0f983603b79d9993f4004f85aa
x1Wolf101x <amsorensen33@gmail.com>
2025-02-24 19:10:02 -0800

    Boss Functionality
    Boss now follows player

--- 383Game/Assets/src/tl2/AS-Scripts/tutorialTestBoss.cs
+++ 383Game/Assets/src/tl2/AS-Scripts/tutorialTestBoss.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class tutorialTestBoss : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    Transform player;
+    Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    void Start(){
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = transform.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+
+        rb.MovePosition(newPos);
     }
 }
