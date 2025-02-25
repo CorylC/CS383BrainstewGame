355be53c01652bf557e6311ba4e7e0110c9fc075
Nihat K Polat <nihatkayrapolat@gmail.com>
2025-02-24 23:51:48 -0800

    Edited the Environment
    added new image for the platform, and adjustments

---
+++ 383Game/Assets/src/tl4/MovingPlatform.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public Transform posA, posB;
+    public float speed;
+    Vector2 targetPos;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetPos = posB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, posB.position) < 0.1f) targetPos = posA.position;
+
+        if(Vector2.Distance(transform.position, posA.position) < 0.1f) targetPos = posB.position;
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(this.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(posB.position, posA.position);
+    }
+}
