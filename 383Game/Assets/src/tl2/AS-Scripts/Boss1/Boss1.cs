99f0cc4c1ad0f36b562618662a40e9a0a972db0d
x1Wolf101x <amsorensen33@gmail.com>
2025-03-19 12:07:26 -0700

    Boss1 Update (3-19-2025/12:07)
    Boss now fires multiple bullets instead of one and turns to face the player when they jump to the other side. However, the boss movement no longer works. I will need to fix that.

--- 383Game/Assets/src/tl2/AS-Scripts/Boss1/Boss1.cs
+++ 383Game/Assets/src/tl2/AS-Scripts/Boss1/Boss1.cs
@@ -3,14 +3,40 @@ using UnityEngine;
 public class Boss1 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public Transform player;
+
+    public bool isFlipped = false;
+
+    public void facePlayer(){
+
+        Vector3 flipped = transform.localScale;
+        flipped.z = -1f;
+
+        if(transform.position.x > player.position.x && isFlipped) { 
+            
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+
+            
+        }else if(transform.position.x < player.position.x && !isFlipped){
+
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+
+        }
+
     }
 }
--- 383Game/Assets/src/tl2/AS-Scripts/Boss1/Boss1Attack.cs
+++ 383Game/Assets/src/tl2/AS-Scripts/Boss1/Boss1Attack.cs
@@ -6,33 +6,41 @@ public class Boss1Attack : MonoBehaviour
     public int atkDmg = 25;
     public int atkDmg2 = 34;
 
-    public float fireRate = 1f;
+    public float fireRate = 15000f;
     private float timeToFire = 0f;
+
+    private float timer;
 
     public Transform Boss1Gun;
     public GameObject bulletPrefab;
 
     public void bossAtk1(){
 
-        if(timeToFire <= 0f){
-            Debug.Log("Firing");
+        //if(timeToFire <= 0f){
+            //Debug.Log("Boss Firing");
             Instantiate(bulletPrefab, Boss1Gun.position, Boss1Gun.rotation);
-            timeToFire = fireRate;
-        }else{
-            timeToFire -= Time.deltaTime;
+            //timeToFire = fireRate;
+        //}else{
+        //    timeToFire -= Time.deltaTime;
 
-        }
+        //}
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
+    void Start()
+    {
         
-    //}
+    }
 
     // Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 0.5){
+
+            timer = 0;
+            bossAtk1();
+        }
+    }
 }
