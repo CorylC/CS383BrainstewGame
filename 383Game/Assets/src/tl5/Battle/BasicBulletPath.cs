2824a157321845c029d176a055caafe4891ac7b6
Aneesha-Shr <109557810+Aneesha-Shr@users.noreply.github.com>
2025-04-02 12:57:55 -0700

    Added Enemies to Amazoon

---
+++ 383Game/Assets/src/tl5/Battle/BasicBulletPath.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class BasicBulletPath : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 15f; // Fixed speed for horizontal movement
+    private float timer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        // Move the projectile in a fixed horizontal direction (right or left)
+        rb.linearVelocity = new Vector2(speed, 0); // Moves right. Use -speed for left.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 10){
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player")){
+            Destroy(gameObject);
+        }
+    }
+}
