d5fbc08a0b52e31dd8582b79f3931649c1736312
Aneesha-Shr <109557810+Aneesha-Shr@users.noreply.github.com>
2025-04-02 20:40:52 -0700

    Finished Amazoon From Enemy Side

--- 383Game/Assets/src/tl5/Movement/Patrol.cs
+++ 383Game/Assets/src/tl5/Movement/Patrol.cs
@@ -8,33 +8,48 @@ public class Patrol : MonoBehaviour
     private Animator anim;
     private Transform currentPoint;
     public float speed;
+    private bool facingRight = true;
 
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         currentPoint = PointA.transform;
         anim.SetBool("isRunning", true);
+        
     }
 
     void Update()
     {
         Vector2 point = currentPoint.position - transform.position;
-        if(currentPoint == PointA.transform){
+        if(currentPoint == PointB.transform){
             rb.linearVelocity = new Vector2(speed, 0);
+             if (!facingRight) Flip();
         }
         else
         {
             rb.linearVelocity = new Vector2(-speed, 0);
+             if (facingRight) Flip();
         }
 
-        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
-        {
-            currentPoint = PointB.transform;
-        }
         if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
         {
             currentPoint = PointA.transform;
         }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+        {
+            currentPoint = PointB.transform;
+        }
+
+        
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1; // Invert X scale
+        transform.localScale = scale;
     }
 }
